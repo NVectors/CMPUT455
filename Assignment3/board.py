6e57ca12ead7be6f2f2d25d725c90e7f578db7f5
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2021-03-09 14:13:12 -0700

    Remove useless code

--- Assignment3/board.py
+++ Assignment3/board.py
@@ -276,27 +276,8 @@ class GoBoard(object):
             return True
         elif self.board[point] != EMPTY:
             return False
-        # if point == self.ko_recapture:
-        #     return False
-
-        # General case: deal with captures, suicide, and next ko point
-        # opp_color = GoBoardUtil.opponent(color)
-        # in_enemy_eye = self._is_surrounded(point, opp_color)
+        
         self.board[point] = color
-        # single_captures = []
-        # neighbors = self._neighbors(point)
-        # for nb in neighbors:
-        #     if self.board[nb] == opp_color:
-        #         single_capture = self._detect_and_process_capture(nb)
-        #         if single_capture != None:
-        #             single_captures.append(single_capture)
-        # block = self._block_of(point)
-        # if not self._has_liberty(block):  # undo suicide move
-        #     self.board[point] = EMPTY
-        #     return False
-        # self.ko_recapture = None
-        # if in_enemy_eye and len(single_captures) == 1:
-        #     self.ko_recapture = single_captures[0]
         self.current_player = GoBoardUtil.opponent(color)
         self.last2_move = self.last_move
         self.last_move = point
